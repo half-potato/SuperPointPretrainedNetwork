644ef931697fe9e0461c0107b9c0dc41d568178c
Alexander Mai <alexandertmai@gmail.com>
2020-07-07 00:28:12 -0700

    Changes for training

--- model.py
+++ model.py
@@ -4,150 +4,170 @@ import numpy as np
 from . import util
 
 class SuperPointNet(torch.nn.Module):
-  """ Pytorch definition of SuperPoint Network. """
-  def __init__(self):
-    super(SuperPointNet, self).__init__()
-    self.relu = torch.nn.ReLU(inplace=True)
-    self.pool = torch.nn.MaxPool2d(kernel_size=2, stride=2)
-    c1, c2, c3, c4, c5, d1 = 64, 64, 128, 128, 256, 256
-    # Shared Encoder.
-    self.conv1a = torch.nn.Conv2d(1, c1, kernel_size=3, stride=1, padding=1)
-    self.conv1b = torch.nn.Conv2d(c1, c1, kernel_size=3, stride=1, padding=1)
-    self.conv2a = torch.nn.Conv2d(c1, c2, kernel_size=3, stride=1, padding=1)
-    self.conv2b = torch.nn.Conv2d(c2, c2, kernel_size=3, stride=1, padding=1)
-    self.conv3a = torch.nn.Conv2d(c2, c3, kernel_size=3, stride=1, padding=1)
-    self.conv3b = torch.nn.Conv2d(c3, c3, kernel_size=3, stride=1, padding=1)
-    self.conv4a = torch.nn.Conv2d(c3, c4, kernel_size=3, stride=1, padding=1)
-    self.conv4b = torch.nn.Conv2d(c4, c4, kernel_size=3, stride=1, padding=1)
-    # Detector Head.
-    self.convPa = torch.nn.Conv2d(c4, c5, kernel_size=3, stride=1, padding=1)
-    self.convPb = torch.nn.Conv2d(c5, 65, kernel_size=1, stride=1, padding=0)
-    # Descriptor Head.
-    self.convDa = torch.nn.Conv2d(c4, c5, kernel_size=3, stride=1, padding=1)
-    self.convDb = torch.nn.Conv2d(c5, d1, kernel_size=1, stride=1, padding=0)
+    """ Pytorch definition of SuperPoint Network. """
+    def __init__(self):
+        super(SuperPointNet, self).__init__()
+        self.relu = torch.nn.ReLU(inplace=True)
+        self.pool = torch.nn.MaxPool2d(kernel_size=2, stride=2)
+        c1, c2, c3, c4, c5, d1 = 64, 64, 128, 128, 256, 256
+        # Shared Encoder.
+        self.conv1a = torch.nn.Conv2d(1, c1, kernel_size=3, stride=1, padding=1)
+        self.conv1b = torch.nn.Conv2d(c1, c1, kernel_size=3, stride=1, padding=1)
+        self.conv2a = torch.nn.Conv2d(c1, c2, kernel_size=3, stride=1, padding=1)
+        self.conv2b = torch.nn.Conv2d(c2, c2, kernel_size=3, stride=1, padding=1)
+        self.conv3a = torch.nn.Conv2d(c2, c3, kernel_size=3, stride=1, padding=1)
+        self.conv3b = torch.nn.Conv2d(c3, c3, kernel_size=3, stride=1, padding=1)
+        self.conv4a = torch.nn.Conv2d(c3, c4, kernel_size=3, stride=1, padding=1)
+        self.conv4b = torch.nn.Conv2d(c4, c4, kernel_size=3, stride=1, padding=1)
+        # Detector Head.
+        self.convPa = torch.nn.Conv2d(c4, c5, kernel_size=3, stride=1, padding=1)
+        self.convPb = torch.nn.Conv2d(c5, 65, kernel_size=1, stride=1, padding=0)
+        # Descriptor Head.
+        self.convDa = torch.nn.Conv2d(c4, c5, kernel_size=3, stride=1, padding=1)
+        self.convDb = torch.nn.Conv2d(c5, d1, kernel_size=1, stride=1, padding=0)
 
-  def forward(self, x):
-    """ Forward pass that jointly computes unprocessed point and descriptor
-    tensors.
-    Input
-      x: Image pytorch tensor shaped N x 1 x H x W.
-    Output
-      semi: Output point pytorch tensor shaped N x 65 x H/8 x W/8.
-      desc: Output descriptor pytorch tensor shaped N x 256 x H/8 x W/8.
-    """
-    # Shared Encoder.
-    x = self.relu(self.conv1a(x))
-    x = self.relu(self.conv1b(x))
-    x = self.pool(x)
-    x = self.relu(self.conv2a(x))
-    x = self.relu(self.conv2b(x))
-    x = self.pool(x)
-    x = self.relu(self.conv3a(x))
-    x = self.relu(self.conv3b(x))
-    x = self.pool(x)
-    x = self.relu(self.conv4a(x))
-    x = self.relu(self.conv4b(x))
-    # Detector Head.
-    cPa = self.relu(self.convPa(x))
-    semi = self.convPb(cPa)
-    # Descriptor Head.
-    cDa = self.relu(self.convDa(x))
-    desc = self.convDb(cDa)
-    dn = torch.norm(desc, p=2, dim=1) # Compute the norm.
-    desc = desc.div(torch.unsqueeze(dn, 1)) # Divide by norm to normalize.
-    return semi, desc
+        self.cell = 8 # Size of each output cell. Keep this fixed.
+
+    def forward(self, x):
+        """ Forward pass that jointly computes unprocessed point and descriptor
+        tensors.
+        Input
+            x: Image pytorch tensor shaped N x 1 x H x W.
+        Output
+            semi: Output point pytorch tensor shaped N x 65 x H/8 x W/8.
+            desc: Output descriptor pytorch tensor shaped N x 256 x H/8 x W/8.
+        """
+        N, _, H, W = x.shape
+
+        # Shared Encoder.
+        x = self.relu(self.conv1a(x))
+        x = self.relu(self.conv1b(x))
+        x = self.pool(x)
+        x = self.relu(self.conv2a(x))
+        x = self.relu(self.conv2b(x))
+        x = self.pool(x)
+        x = self.relu(self.conv3a(x))
+        x = self.relu(self.conv3b(x))
+        x = self.pool(x)
+        x = self.relu(self.conv4a(x))
+        x = self.relu(self.conv4b(x))
+        # Detector Head.
+        cPa = self.relu(self.convPa(x))
+        semi = self.convPb(cPa)
+        # Descriptor Head.
+        cDa = self.relu(self.convDa(x))
+        desc = self.convDb(cDa)
+        dn = torch.norm(desc, p=2, dim=1) # Compute the norm.
+        desc = desc.div(torch.unsqueeze(dn, 1)) # Divide by norm to normalize.
+        return semi, desc
+
+    def semi_to_heatmap(self, semi, net_input_shape):
+        N, _, H, W = net_input_shape
+        # Reshape detector head
+        dense = torch.exp(semi) # Softmax.
+        dense = dense / (torch.sum(dense, dim=0)+.00001) # Should sum to 1.
+        # Remove dustbin.
+        nodust = dense[:, :-1, :, :] # N x 64 x Hc x Wc
+        # Reshape to get full resolution heatmap.
+        Hc = int(H / self.cell)
+        Wc = int(W / self.cell)
+        nodust = nodust.permute(0, 2, 3, 1) # N x Hc x Wc
+        heatmap = nodust.reshape([N, Hc, Wc, self.cell, self.cell])
+        heatmap = heatmap.permute(0, 1, 3, 2, 4)
+        heatmap = heatmap.reshape([N, H, W])
+        return heatmap
 
 class SuperPointFrontend(object):
-  """ Wrapper around pytorch net to help with pre and post image processing. """
-  def __init__(self, weights_path, nms_dist, conf_thresh, nn_thresh,
-               cuda=False):
-    self.name = 'SuperPoint'
-    self.cuda = cuda
-    self.nms_dist = nms_dist
-    self.conf_thresh = conf_thresh
-    self.nn_thresh = nn_thresh # L2 descriptor distance for good match.
-    self.cell = 8 # Size of each output cell. Keep this fixed.
-    self.border_remove = 4 # Remove points this close to the border.
+    """ Wrapper around pytorch net to help with pre and post image processing. """
+    def __init__(self, weights_path, nms_dist, conf_thresh, nn_thresh,
+                             cuda=False):
+        self.name = 'SuperPoint'
+        self.cuda = cuda
+        self.nms_dist = nms_dist
+        self.conf_thresh = conf_thresh
+        self.nn_thresh = nn_thresh # L2 descriptor distance for good match.
+        self.cell = 8 # Size of each output cell. Keep this fixed.
+        self.border_remove = 4 # Remove points this close to the border.
 
-    # Load the network in inference mode.
-    self.net = SuperPointNet()
-    if cuda:
-      # Train on GPU, deploy on GPU.
-      self.net.load_state_dict(torch.load(weights_path))
-      self.net = self.net.cuda()
-    else:
-      # Train on GPU, deploy on CPU.
-      self.net.load_state_dict(torch.load(weights_path,
-                               map_location=lambda storage, loc: storage))
-    self.net.eval()
+        # Load the network in inference mode.
+        self.net = SuperPointNet()
+        if cuda:
+            # Train on GPU, deploy on GPU.
+            self.net.load_state_dict(torch.load(weights_path))
+            self.net = self.net.cuda()
+        else:
+            # Train on GPU, deploy on CPU.
+            self.net.load_state_dict(torch.load(weights_path,
+                                                             map_location=lambda storage, loc: storage))
+        self.net.eval()
 
 
-  def run(self, img):
-    """ Process a numpy image to extract points and descriptors.
-    Input
-      img - HxW numpy float32 input image in range [0,1].
-    Output
-      corners - 3xN numpy array with corners [x_i, y_i, confidence_i]^T.
-      desc - 256xN numpy array of corresponding unit normalized descriptors.
-      heatmap - HxW numpy heatmap in range [0,1] of point confidences.
-      """
-    assert img.ndim == 2, 'Image must be grayscale.'
-    assert img.dtype == np.float32, 'Image must be float32.'
-    H, W = img.shape[0], img.shape[1]
-    inp = img.copy()
-    inp = (inp.reshape(1, H, W))
-    inp = torch.from_numpy(inp)
-    inp = torch.autograd.Variable(inp).view(1, 1, H, W)
-    if self.cuda:
-      inp = inp.cuda()
-    # Forward pass of network.
-    outs = self.net.forward(inp)
-    semi, coarse_desc = outs[0], outs[1]
-    # Convert pytorch -> numpy.
-    semi = semi.data.cpu().numpy().squeeze()
-    # --- Process points.
-    dense = np.exp(semi) # Softmax.
-    dense = dense / (np.sum(dense, axis=0)+.00001) # Should sum to 1.
-    # Remove dustbin.
-    nodust = dense[:-1, :, :]
-    # Reshape to get full resolution heatmap.
-    Hc = int(H / self.cell)
-    Wc = int(W / self.cell)
-    nodust = nodust.transpose(1, 2, 0)
-    heatmap = np.reshape(nodust, [Hc, Wc, self.cell, self.cell])
-    heatmap = np.transpose(heatmap, [0, 2, 1, 3])
-    heatmap = np.reshape(heatmap, [Hc*self.cell, Wc*self.cell])
-    xs, ys = np.where(heatmap > self.conf_thresh) # Confidence threshold.
-    if len(xs) == 0:
-      return np.zeros((3, 0)), None, None
-    pts = np.zeros((3, len(xs))) # Populate point data sized 3xN.
-    pts[0, :] = ys
-    pts[1, :] = xs
-    pts[2, :] = heatmap[xs, ys]
-    pts, _ = util.nms_fast(pts, H, W, dist_thresh=self.nms_dist) # Apply NMS.
-    inds = np.argsort(pts[2,:])
-    pts = pts[:,inds[::-1]] # Sort by confidence.
-    # Remove points along border.
-    bord = self.border_remove
-    toremoveW = np.logical_or(pts[0, :] < bord, pts[0, :] >= (W-bord))
-    toremoveH = np.logical_or(pts[1, :] < bord, pts[1, :] >= (H-bord))
-    toremove = np.logical_or(toremoveW, toremoveH)
-    pts = pts[:, ~toremove]
-    # --- Process descriptor.
-    D = coarse_desc.shape[1]
-    if pts.shape[1] == 0:
-      desc = np.zeros((D, 0))
-    else:
-      # Interpolate into descriptor map using 2D point locations.
-      samp_pts = torch.from_numpy(pts[:2, :].copy())
-      samp_pts[0, :] = (samp_pts[0, :] / (float(W)/2.)) - 1.
-      samp_pts[1, :] = (samp_pts[1, :] / (float(H)/2.)) - 1.
-      samp_pts = samp_pts.transpose(0, 1).contiguous()
-      samp_pts = samp_pts.view(1, 1, -1, 2)
-      samp_pts = samp_pts.float()
-      if self.cuda:
-        samp_pts = samp_pts.cuda()
-      desc = torch.nn.functional.grid_sample(coarse_desc, samp_pts)
-      desc = desc.data.cpu().numpy().reshape(D, -1)
-      desc /= np.linalg.norm(desc, axis=0)[np.newaxis, :]
-    return pts, desc, heatmap
+    def run(self, img):
+        """ Process a numpy image to extract points and descriptors.
+        Input
+            img - HxW numpy float32 input image in range [0,1].
+        Output
+            corners - 3xN numpy array with corners [x_i, y_i, confidence_i]^T.
+            desc - 256xN numpy array of corresponding unit normalized descriptors.
+            heatmap - HxW numpy heatmap in range [0,1] of point confidences.
+            """
+        assert img.ndim == 2, 'Image must be grayscale.'
+        assert img.dtype == np.float32, 'Image must be float32.'
+        H, W = img.shape[0], img.shape[1]
+        inp = img.copy()
+        inp = (inp.reshape(1, H, W))
+        inp = torch.from_numpy(inp)
+        inp = torch.autograd.Variable(inp).view(1, 1, H, W)
+        if self.cuda:
+            inp = inp.cuda()
+        # Forward pass of network.
+        outs = self.net.forward(inp)
+        semi, coarse_desc = outs[0], outs[1]
+        # Convert pytorch -> numpy.
+        semi = semi.data.cpu().numpy().squeeze()
+        # --- Process points.
+        dense = np.exp(semi) # Softmax.
+        dense = dense / (np.sum(dense, axis=0)+.00001) # Should sum to 1.
+        # Remove dustbin.
+        nodust = dense[:-1, :, :]
+        # Reshape to get full resolution heatmap.
+        Hc = int(H / self.cell)
+        Wc = int(W / self.cell)
+        nodust = nodust.transpose(1, 2, 0)
+        heatmap = np.reshape(nodust, [Hc, Wc, self.cell, self.cell])
+        heatmap = np.transpose(heatmap, [0, 2, 1, 3])
+        heatmap = np.reshape(heatmap, [Hc*self.cell, Wc*self.cell])
+        xs, ys = np.where(heatmap > self.conf_thresh) # Confidence threshold.
+        if len(xs) == 0:
+            return np.zeros((3, 0)), None, None
+        pts = np.zeros((3, len(xs))) # Populate point data sized 3xN.
+        pts[0, :] = ys
+        pts[1, :] = xs
+        pts[2, :] = heatmap[xs, ys]
+        pts, _ = util.nms_fast(pts, H, W, dist_thresh=self.nms_dist) # Apply NMS.
+        inds = np.argsort(pts[2,:])
+        pts = pts[:,inds[::-1]] # Sort by confidence.
+        # Remove points along border.
+        bord = self.border_remove
+        toremoveW = np.logical_or(pts[0, :] < bord, pts[0, :] >= (W-bord))
+        toremoveH = np.logical_or(pts[1, :] < bord, pts[1, :] >= (H-bord))
+        toremove = np.logical_or(toremoveW, toremoveH)
+        pts = pts[:, ~toremove]
+        # --- Process descriptor.
+        D = coarse_desc.shape[1]
+        if pts.shape[1] == 0:
+            desc = np.zeros((D, 0))
+        else:
+            # Interpolate into descriptor map using 2D point locations.
+            samp_pts = torch.from_numpy(pts[:2, :].copy())
+            samp_pts[0, :] = (samp_pts[0, :] / (float(W)/2.)) - 1.
+            samp_pts[1, :] = (samp_pts[1, :] / (float(H)/2.)) - 1.
+            samp_pts = samp_pts.transpose(0, 1).contiguous()
+            samp_pts = samp_pts.view(1, 1, -1, 2)
+            samp_pts = samp_pts.float()
+            if self.cuda:
+                samp_pts = samp_pts.cuda()
+            desc = torch.nn.functional.grid_sample(coarse_desc, samp_pts)
+            desc = desc.data.cpu().numpy().reshape(D, -1)
+            desc /= np.linalg.norm(desc, axis=0)[np.newaxis, :]
+        return pts, desc, heatmap
